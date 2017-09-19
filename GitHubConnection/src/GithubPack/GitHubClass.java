2c1c3a9687c5f7da38b8c6dafde89f9ca0e1735e
goelvi <goelvi@INGUEUCLP000462.camkmg-bangalore.com>
2017-09-19 10:45:43 +0530

    Commit done by Vikas on 19th Sept 2017

---
+++ GitHubConnection/src/GithubPack/GitHubClass.java
@@ -0,0 +1,35 @@
+package GithubPack;
+
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GitHubClass {
+	public static void main(String[] args) throws Exception {
+		WebDriver driver=new FirefoxDriver();
+
+		driver.get("https://www.redbus.in/");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath(".//*[@id='search']/div/div[3]/div/label")).click();
+					
+		List<WebElement> allDates=driver.findElements(By.xpath("//div[@id='rb-calendar_onward_cal']//td"));
+		
+		
+		int total_node = allDates.size();
+		
+		for(int i=0;i<total_node;i++)
+		{
+			String date=allDates.get(i).getText();
+			
+			if(date.equalsIgnoreCase("30"))
+			{
+				allDates.get(i).click();
+				break;
+			}
+			
+		}
+	}
+}
